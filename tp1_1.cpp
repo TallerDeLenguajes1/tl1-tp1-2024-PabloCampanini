047d11dd82435bc5dc9adc6a8a3c423aa5d7a5a8
PabloCampanini <pablocampanini00@gmail.com>
2024-04-08 01:55:14 -0300

    Modificion de punteros

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -2,7 +2,19 @@
 
 int main()
 {
-    printf("Hola mundo");
+    printf("Hola mundo\n");
+
+    //Declaracion de variables
+    int numero = 100;
+
+    //Declaracion de punteros
+    int *pnumero = &numero;
+
+    printf("El contenido del puntero: %d\n", *pnumero);
+    printf("La direccion de memoria almacenada por el puntero: %p\n", pnumero);
+    printf("La direccion de memoria de la variable: %p\n", &numero);
+    printf("La direccion de memoria del puntero: %p\n", &pnumero);
+    printf("El tamanio de memoria del entero es: %zu bytes\n", sizeof(int)); 
 
     return 0;
 }
